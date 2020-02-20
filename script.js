c9199e351df944ebf379d4c2d81727d9c91663fc
MaryHooker <mc020791@yahoo.com>
2020-02-20 14:20:17 -0600

    while A and for 1 loop

---
+++ script.js
@@ -0,0 +1,17 @@
+// // ### Exercise 1:
+// // - Use a for loop to print numbers -3 through 8
+
+// //variable set to -3 inside for loop/condition(as long as number is less than or equal to 8 continue loop)/keep adding to number 
+// for(let numbers=-3;numbers<=8;numbers++){
+//     console.log(numbers)
+// }
+//***************************************************
+// // ### Exercise A:
+// // - Print -10 through 5 using a counter and while loop.
+
+// let kount=-10
+// while(kount<=5){
+//     console.log(kount)
+//     kount++
+// }
+//*****************************************************
