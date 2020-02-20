86f5e5c707ff575dca56581ab2037d83cffaeef6
MaryHooker <mc020791@yahoo.com>
2020-02-20 15:05:12 -0600

    Loops

--- script.js
+++ script.js
@@ -15,3 +15,45 @@
 //     kount++
 // }
 //*****************************************************
+// // ### Exercise 2:
+// // - Use a for loop to count from -5 to 5 three times in a row
+
+// for(let kount=-5;kount<=5;kount++{
+//     console.log(kount)
+    
+// }
+//*****not finished
+//*****************************************************
+// // ### Exercise B:
+// // - Write a While loop that counts backwards from 10 to 0
+// // - Log the current count at the end of each iteration EXCEPT if the current count is ```5```
+
+// let count=10
+// while(count>=0){
+//     console.log(count)
+//     count--
+// }
+//*****not finished
+//****************************************************
+// // ### Exercise 3:
+// // - Use a for loop to print only even numbers from -50 to 50
+
+// for(let numbers=-50;numbers<=50;numbers=numbers+2)
+// {
+//     console.log(numbers)
+// }
+//****************************************************
+// // ### Exercise C:
+// // - Create an empty `name_array`
+// let name_array=[""]
+// // - Ask the user ```Enter a word. Enter 'q' to quit.``` Add each word they enter to the array
+// let userName=prompt("Please enter a word. Enter 'q' to quit.")
+//  while(userName!=q){
+//     name_array.push(userName)
+//  } 
+//  console.log(name_array)
+// // - Create a while loop that will keep asking for words to add to the array until they enter 'q'
+// // - When the user enters 'q' use a **for** loop to print each word from the array
+
+//*********not finished
+//******************************************************
